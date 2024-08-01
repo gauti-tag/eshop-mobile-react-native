561dd60d8837ff81764ba44657f50f97f3296485
gauti-tag <gautier.tiehoule@gmail.com>
2024-08-01 13:51:44 +0000

    Feat: Create product component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import screens
+import ProductContainer from './Screens/Products/ProductContainer';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductContainer />
     </View>
   );
 }
