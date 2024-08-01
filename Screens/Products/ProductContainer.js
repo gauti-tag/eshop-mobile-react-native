1049d2782badb760a6d48b1f70fb85203fa0077b
gauti-tag <gautier.tiehoule@gmail.com>
2024-08-01 14:57:42 +0000

    Feat: Setup Products FlatList

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from "react-native";
+
+const data = require('../../assets/data/products.json');
 
 const ProductContainer = () => {
+    // Manage products state
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(data);
+
+        return () => {
+            setProducts([]);
+        }
+    }, []);
+
     return (
         <View>
             <Text>
                 Product container
             </Text>
+
+            <View style={{ marginTop: 100 }}>
+                <FlatList
+                    horizontal
+                    data={products}
+                    renderItem={({ item }) => <Text>{item.brand}</Text>}
+                    keyExtractor={item => item.name}
+                />
+            </View>
         </View>
     )
 }
